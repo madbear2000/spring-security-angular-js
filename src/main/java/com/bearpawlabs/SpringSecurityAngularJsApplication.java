269f56be5af4d06e7217be835768ff6067fab805
Andrew Vaughan <avaughan@afilias.info>
2016-12-02 16:44:10 +0000

    Basic structure no security or resources

---
+++ src/main/java/com/bearpawlabs/SpringSecurityAngularJsApplication.java
@@ -0,0 +1,12 @@
+package com.bearpawlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAngularJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAngularJsApplication.class, args);
+	}
+}
---
+++ src/main/resources/static/js/hello.js
@@ -0,0 +1,6 @@
+angular.module('hello', [])
+  .controller('home', function($scope) {
+	  
+	  ctrl = this;
+	  ctrl.greeting = {id: 'xxx', content: 'Hello World!'}
+})
